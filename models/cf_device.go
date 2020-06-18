03829eaba16e6e50301f7afeda64f5e5e813a655
snowlyg <569616226@qq.com>
2020-06-18 15:44:33 +0800

    修改配置，去除端口

--- models/cf_device.go
+++ models/cf_device.go
@@ -1,7 +1,5 @@
 package models
 
-import "time"
-
 //dev_id as local_device_id,
 //dev_code as device_code,
 //dev_desc as device_desc,
@@ -12,22 +10,12 @@ import "time"
 //dev_create_time as create_at
 
 type CfDevice struct {
-	DevId         int64     `json:"local_device_id" gorm:"column:dev_id"`                        // 服务id
-	DevType       int64     `json:"device_type_id" gorm:"column:dev_type"`                       // 类型
-	DevActive     int64     `json:"device_active" gorm:"column:dev_active"`                      // 状态
-	DevCode       string    `json:"device_code" gorm:"column:dev_code;type:varchar(50)"`         // 设备代码
-	DevDesc       string    `json:"device_desc" gorm:"column:dev_desc;type:varchar(50)"`         // 设备描述
-	DevPosition   string    `json:"device_position" gorm:"column:dev_position;type:varchar(50)"` // 位置
-	DevIp         string    `json:"device_ip" gorm:"column:dev_ip;type:varchar(40)"`             // ip
-	DevCreateTime time.Time `json:"create_at" gorm:"column:dev_create_time"`                     // 创建时间
-	//
-	//CtLocId            int64
-	//PacRoomId          int64
-	//DevStatus          int64
-	//DevBindId          int64
-	//CfDeviceSoftwareId int64
-	//DevSleepTime       time.Time
-	//DevStartupTime     time.Time
-	//SipUsersName       string
-	//DevBindChat        string
+	DevId         int64  `json:"local_device_id" gorm:"column:dev_id"`                        // 服务id
+	DevType       int64  `json:"device_type_id" gorm:"column:dev_type"`                       // 类型
+	DevActive     int64  `json:"device_active" gorm:"column:dev_active"`                      // 状态
+	DevCode       string `json:"device_code" gorm:"column:dev_code;type:varchar(50)"`         // 设备代码
+	DevDesc       string `json:"device_desc" gorm:"column:dev_desc;type:varchar(50)"`         // 设备描述
+	DevPosition   string `json:"device_position" gorm:"column:dev_position;type:varchar(50)"` // 位置
+	DevIp         string `json:"device_ip" gorm:"column:dev_ip;type:varchar(40)"`             // ip
+	DevCreateTime string `json:"create_at" gorm:"column:dev_create_time"`                     // 创建时间
 }
