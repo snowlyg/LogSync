e4f55b94197e433db92702f0fef8f8e882e7ee40
snowlyg <569616226@qq.com>
2020-08-14 15:14:13 +0800

    v1.6 调整日志为一次性提交

--- main.go
+++ main.go
@@ -115,6 +115,7 @@ func syncDeviceLog() {
 			sync.LogMsgs = nil
 			sync.LogCodes = nil
 			sync.ServerMsgs = nil
+			sync.ServerNames = nil
 			sync.CheckService()
 			// 进入当天目录,跳过 23点45 当天凌晨 0点15 分钟，给设备创建目录的时间
 			if !((time.Now().Hour() == 0 && time.Now().Minute() < 15) || (time.Now().Hour() == 23 && time.Now().Minute() > 45)) {
